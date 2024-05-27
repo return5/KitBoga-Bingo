a8c86a94740258ea775040c74508c0391db54951
chris <armoredmoose@gmail.com>
2024-05-26 20:01:32 -0500

    set up restart

--- main.lua
+++ main.lua
@@ -3,23 +3,23 @@ local TilesFactory = require('factory.TilesFactory')
 local CanvasFactory = require('factory.CanvasFactory')
 local ClickableFactory = require('factory.ClickableFactory')
 
-local startY = 100
-local lineWidth = 1
-
-local function generateCanvases(tiles)
+local function initBoard()
+	local startY = 100
+	local lineWidth = 1
+	local tiles = TilesFactory.generateTiles(100,100,GridText.getRandomTextArray(),lineWidth + 1,startY + lineWidth,lineWidth)
 	local height = tiles.tiles[#tiles.tiles].y_height + lineWidth
 	local width = tiles.tiles[#tiles.tiles].x_width + lineWidth
 	love.window.setMode(width,height)
-	return {
+	local clickable = ClickableFactory.createClickable(tiles)
+	local canvases = {
 		CanvasFactory.createTextCanvas(tiles),
-		CanvasFactory.createLinesCanvas(tiles,startY,lineWidth),
-		CanvasFactory.createTitle("Kit Boga Bingo",1,1,startY + lineWidth,width)
+		CanvasFactory.createLinesCanvas(tiles, startY, lineWidth),
+		CanvasFactory.createTitle("Kit Boga Bingo", 1, 1, startY + lineWidth, width)
 	}
+	return clickable,canvases
 end
 
-local tiles = TilesFactory.generateTiles(100,100,GridText.getRandomTextArray(),lineWidth + 1,startY + lineWidth,lineWidth)
-local clickable = ClickableFactory.createClickable(tiles)
-local canvases = generateCanvases(tiles)
+local clickable,canvases = initBoard()
 
 function love.draw()
 	for i=1,#canvases,1 do
@@ -28,12 +28,17 @@ function love.draw()
 	clickable:print()
 end
 
-function love.mousepressed( x, y, button )
+function love.mousepressed(x,y,button)
 	if button == 1 then
 		clickable:clicked(x,y)
 	end
 end
 
+function love.keypressed(key)
+	if key == "r" then
+		clickable,canvases = initBoard()
+	end
+end
 
 function love.update(dt)
 
--- model/XMark.lua
+++ model/XMark.lua
@@ -4,9 +4,7 @@ local setmetatable = setmetatable
 local XMark = {}
 XMark.__index = XMark
 
-
 _ENV = XMark
-
 
 function XMark:print()
 	line(self.x1,self.y1,self.x2,self.y2)
